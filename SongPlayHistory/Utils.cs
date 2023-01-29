43e35b794e09960d1ed9e7643a5a2454d6dc7329
Sky Z <qe201020335@sina.com>
2023-01-29 15:23:25 -0500

    fix: BeatLeader replay detection

--- SongPlayHistory/Utils.cs
+++ SongPlayHistory/Utils.cs
@@ -9,45 +9,25 @@ namespace SongPlayHistoryContinued
 {
     internal static class Utils
     {
-        #region replay check (copied from HRCounter, MIT LICENSE: https://github.com/qe201020335/HRCounter/blob/master/LICENSE)
-        private const string BEATLEADER_MOD_ID = "BeatLeader";
-
-        private static bool? _beatleaderHasReplay = null;
-
-        internal static bool BeatLeaderHasReplay
-        {
-            get
-            {
-                var blVersion = FindEnabledPluginMetadata(BEATLEADER_MOD_ID)?.HVersion;
-                _beatleaderHasReplay ??= blVersion != null && blVersion >= new Version(0, 5, 0);
-                return _beatleaderHasReplay.Value;
-            }
-        }
-
+        #region replay check (copied from HRCounter) 
+        // MIT LICENSE: https://github.com/qe201020335/HRCounter/blob/master/LICENSE)
         // copied from Camera2
-        private static readonly MethodBase? ScoreSaber_playbackEnabled =
+         private static readonly MethodBase? ScoreSaber_playbackEnabled =
             AccessTools.Method("ScoreSaber.Core.ReplaySystem.HarmonyPatches.PatchHandleHMDUnmounted:Prefix");
+
+        private static readonly MethodBase? GetBeatLeaderIsStartedAsReplay =
+            AccessTools.Property(AccessTools.TypeByName("BeatLeader.Replayer.ReplayerLauncher"), "IsStartedAsReplay")?.GetGetMethod(false);
+
         
         internal static bool IsInReplay()
         {
             // copied from Camera2
             var ssReplay = ScoreSaber_playbackEnabled != null && (bool) ScoreSaber_playbackEnabled.Invoke(null, null) == false;
 
-            var blReplay = BeatLeaderHasReplay && ReplayerLauncher.IsStartedAsReplay;
+            var blReplay = GetBeatLeaderIsStartedAsReplay != null && (bool) GetBeatLeaderIsStartedAsReplay.Invoke(null, null);
             
             return ssReplay || blReplay;
         }
-        #endregion replay
-        
-        internal static bool IsModEnabled(string id)
-        {
-            return FindEnabledPluginMetadata(id) != null;
-        }
-        
-        internal static PluginMetadata? FindEnabledPluginMetadata(string id)
-        {
-            return PluginManager.EnabledPlugins.FirstOrDefault(x => x.Id == id);
-        }
-
+        #endregion
     }
 }
